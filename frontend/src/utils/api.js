0aac5bcbe3c37016e8aad952f8b96e0f92e88264
mahadevatishay <atisheyjain171@gmail.com>
2026-04-25 23:25:28 +0530

    Fix 404 on refresh for React Router

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/' })
+const api = axios.create({
+  baseURL: 'https://phishguard-backend-d861.onrender.com',
+})
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('pg_token')
